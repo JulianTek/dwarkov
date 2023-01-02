7e91f9a3a5de1a0e50a7ba2cf84f6e29ad001df6
Julian <Julian-Tekstra@hotmail.com>
2023-01-02 23:09:56 +0100

    Corridor first-generation algorithm

--- Dwarf Tarkov/Assets/Project/Scripts/World Generation/RoomGenerator.cs
+++ Dwarf Tarkov/Assets/Project/Scripts/World Generation/RoomGenerator.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using Random = UnityEngine.Random;
+using System;
 
 namespace WorldGeneration
 {
     public class RoomGenerator : MonoBehaviour
     {
         private WalkGenerator walkGenerator;
+        private CorridorGenerator corridorGenerator;
         [SerializeField]
         private TileMapVisualizer tileMapVisualizer;
 
@@ -18,9 +20,18 @@ namespace WorldGeneration
         [SerializeField]
         private int iterations = 10;
         [SerializeField]
-        private int length = 10;
+        private int roomLength = 10;
         [SerializeField]
         private bool randomPositionEachIteration = true;
+
+
+        [SerializeField]
+        [Range(0.1f, 1f)]
+        private float roomChance;
+        [SerializeField]
+        private int corridorLength, corridorCount;
+        [SerializeField]
+        private bool roomFirst;
 
         private void Start()
         {
@@ -29,25 +40,90 @@ namespace WorldGeneration
 
         public void Generate()
         {
-            HashSet<Vector2Int> positions = RunGenerationAlgorithm();
+            walkGenerator = new WalkGenerator();
+            corridorGenerator = new CorridorGenerator();
             tileMapVisualizer.Clear();
+            if (roomFirst)
+            {
+                // Will generate room-first-based world
+                GenerateRoom();
+            }
+            else
+            {
+                // Will generate corridor-first-based world
+                CorridorFirstGeneration();
+            }
+        }
+
+        private void CorridorFirstGeneration()
+        {
+            HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
+            HashSet<Vector2Int> roomPositions = new HashSet<Vector2Int>();
+
+            //Generates and visualizes the corridors
+            GenerateCorridors(floorPositions, roomPositions);
+
+            var rooms = CreateRooms(roomPositions);
+
+            floorPositions.UnionWith(rooms);
+
+            tileMapVisualizer.PaintFloorTiles(floorPositions);
+            WallGenerator.PaintWallTiles(tileMapVisualizer, floorPositions);
+        }
+
+        private HashSet<Vector2Int> CreateRooms(HashSet<Vector2Int> roomPositions)
+        {
+            HashSet<Vector2Int> rooms = new HashSet<Vector2Int>();
+            int amountOfRooms = Mathf.RoundToInt(roomPositions.Count * roomChance);
+
+            List<Vector2Int> roomsToCreate = roomPositions.OrderBy(x => Guid.NewGuid()).Take(amountOfRooms).ToList();
+
+            foreach (var room in roomsToCreate)
+            {
+                var roomFloor = walkGenerator.Generate(room, roomLength);
+                rooms.UnionWith(roomFloor);
+            }
+
+            return rooms;
+        }
+
+        private void GenerateRoom()
+        {
+            HashSet<Vector2Int> positions = RunGenerationAlgorithm();
             tileMapVisualizer.PaintFloorTiles(positions);
             WallGenerator.PaintWallTiles(tileMapVisualizer, positions);
-        } 
+        }
 
         public HashSet<Vector2Int> RunGenerationAlgorithm()
         {
-            walkGenerator = new WalkGenerator();
             var currentPosition = startPosition;
             HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
             for (int i = 0; i < iterations; i++)
             {
-                var path = walkGenerator.Generate(startPosition, length);
+                var path = walkGenerator.Generate(currentPosition, roomLength);
                 floorPositions.UnionWith(path);
                 if (randomPositionEachIteration)
                     currentPosition = floorPositions.ElementAt(Random.Range(0, floorPositions.Count));
             }
             return floorPositions;
+        }
+
+        private void GenerateCorridors(HashSet<Vector2Int> floorpositions, HashSet<Vector2Int> roomPositions)
+        {
+            var currentPos = startPosition;
+            roomPositions.Add(currentPos);
+            for (int i = 0; i < corridorCount; i++)
+            {
+                var corridor = corridorGenerator.Generate(currentPos, corridorLength) as List<Vector2Int>;
+                currentPos = corridor[corridor.Count - 1];
+                roomPositions.Add(currentPos);
+                floorpositions.UnionWith(corridor);
+            }
+        }
+
+        public void Clear()
+        {
+            tileMapVisualizer.Clear();
         }
     }
 }
