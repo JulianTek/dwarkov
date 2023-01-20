ce0a8fb958c865bcd31a7fda52dfe6e15bdf8256
Julian <Julian-Tekstra@hotmail.com>
2023-01-20 09:48:45 +0100

    Change FOV

--- Dwarf Tarkov/Assets/Project/Scripts/Enemy/EnemyFieldOfView.cs
+++ Dwarf Tarkov/Assets/Project/Scripts/Enemy/EnemyFieldOfView.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class EnemyFieldOfView : MonoBehaviour
 {
+    [SerializeField]
+    private LayerMask layerMask;
+
     [SerializeField]
     private float fov = 90f;
     [SerializeField]
@@ -14,13 +18,27 @@ public class EnemyFieldOfView : MonoBehaviour
     private float angleIncrease;
     [SerializeField]
     private float viewDistance = 50f;
+
+    private Mesh mesh;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 origin = Vector3.zero;
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;  
+    }
+
+    private Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(MathF.Cos(angleRad), MathF.Sin(angleRad));
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        angle = 0f;
+        Vector3 origin = transform.position;
         angleIncrease = fov / rayCount;
-        Mesh mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
 
 
         Vector3[] vertices = new Vector3[rayCount + 2];
@@ -35,13 +53,15 @@ public class EnemyFieldOfView : MonoBehaviour
         {
             Vector3 vertex;
             RaycastHit2D hit = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance);
+            Debug.DrawRay(origin, GetVectorFromAngle(angle), Color.green);
+            Debug.Log(origin);
             if (!hit.collider)
             {
                 vertex = origin + GetVectorFromAngle(angle) * viewDistance;
             }
             else
             {
-                vertex = hit.point;
+                vertex = hit.point; 
             }
             vertices[vertexIndex] = vertex;
 
@@ -53,24 +73,13 @@ public class EnemyFieldOfView : MonoBehaviour
 
                 triangleIndex += 3;
             }
-
-            angle -= angleIncrease;
             vertexIndex++;
+            angle -= angleIncrease;
+
         }
 
         mesh.vertices = vertices;
-        mesh.triangles = triangles;
         mesh.uv = uv;
-    }
-
-    private Vector3 GetVectorFromAngle(float angle)
-    {
-        float angleRad = angle * (Mathf.PI / 180f);
-        return new Vector3(MathF.Cos(angleRad), MathF.Sin(angleRad));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        mesh.triangles = triangles;
     }
 }
