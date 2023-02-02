cb8da9942455c15f7dc83736836f1b33a595791f
Julian <Julian-Tekstra@hotmail.com>
2023-02-02 21:26:33 +0100

    Fix bedroom door

--- Dwarf Tarkov/Assets/Project/Scripts/Outpost/OutpostDoorHandler.cs
+++ Dwarf Tarkov/Assets/Project/Scripts/Outpost/OutpostDoorHandler.cs
@@ -6,13 +6,10 @@ using Sirenix.OdinInspector;
 
 public class OutpostDoorHandler : MonoBehaviour
 {
-    [HorizontalGroup("Stuff")]
+    [HorizontalGroup("Stuff", LabelWidth = 60)]
     [BoxGroup("Stuff/Transforms", LabelText = "Transforms")]
     [SerializeField]
     private Vector3 bedroomTransform;
-    [BoxGroup("Stuff/Transforms", LabelText = "Transforms")]
-    [SerializeField]
-    private Vector3 outpostTransform;
     [BoxGroup("Stuff/TileInfo", LabelText = "TileInfo")]
     [SerializeField]
     private Tilemap doorTileMap;
@@ -22,16 +19,46 @@ public class OutpostDoorHandler : MonoBehaviour
     [BoxGroup("Stuff/TileInfo", LabelText = "TileInfo")]
     [SerializeField]
     private TileBase doorOpen;
+    private bool doorOpened;
+    private Vector3Int doorPosition;
+
+    private void Start()
+    {
+        doorOpened = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.GetComponent<PlayerInputHandler>())
+        if (other.GetComponent<PlayerInputHandler>() && !doorOpened)
         {
-            doorTileMap.SetTile(Vector3Int.FloorToInt(transform.position), doorOpen);
+            Vector3Int pos = Vector3Int.FloorToInt(other.transform.position);
+            Vector3Int doorPos = new Vector3Int(pos.x, pos.y + 1);
+            doorPosition = doorPos;
+            SetTile(doorOpen, doorPosition);
+            StartCoroutine(TransformPlayer(other.gameObject, bedroomTransform));
+            doorOpened = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        
+        if (other.GetComponent<PlayerInputHandler>())
+        {
+            StopAllCoroutines();
+            SetTile(doorClosed, doorPosition);
+            doorOpened = false;
+        }
+    }
+
+    private IEnumerator TransformPlayer(GameObject player, Vector3 pos)
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        player.transform.position = pos;
+    }
+
+    private void SetTile(TileBase tile, Vector3Int pos)
+    {
+        doorTileMap.SetTile(pos, tile);
+        doorTileMap.RefreshAllTiles();
     }
 }
