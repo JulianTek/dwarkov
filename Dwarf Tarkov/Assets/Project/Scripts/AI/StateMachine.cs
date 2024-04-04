d8a430fb13f2e6c2a89d1c440d28c6a212fe0080
Julian <Julian-Tekstra@hotmail.com>
2024-04-04 11:38:17 +0200

    Add comments to some scripts

--- Dwarf Tarkov/Assets/Project/Scripts/AI/StateMachine.cs
+++ Dwarf Tarkov/Assets/Project/Scripts/AI/StateMachine.cs
@@ -11,11 +11,6 @@ namespace AI
         protected GameState nextState;
         protected bool exitStateNextFrame;
 
-        private void Start()
-        {
-            exitStateNextFrame = false;
-        }
-
         private void SwitchState(GameState state)
         {
             exitStateNextFrame = true;
@@ -23,16 +18,25 @@ namespace AI
         }
         public void SwitchState<T>(GameObject owner) where T : GameState
         {
+            // Switches state to a new state. Use new <x>() where <x> is one of the GameState classes
             SwitchState((T)Activator.CreateInstance(typeof(T), owner));
+        }
+
+        // This is the same as a monobehaviour's start/update/stop loop. Every state also has their own start/update/stop function which will be called at appropriate times
+        private void Start()
+        {
+            exitStateNextFrame = false;
         }
 
         protected void Update()
         {
             currentState.Update();
 
+            // if not exiting the current state next frame, do nothing
             if (!exitStateNextFrame)
                 return;
 
+            // otherwise stop the current state, set the state to "nextState", run the start method and continue
             currentState.Stop();
             currentState = nextState;
             currentState.Start();
--- Dwarf Tarkov/Assets/Project/Scripts/Bartering/SellboxHandler.cs
+++ Dwarf Tarkov/Assets/Project/Scripts/Bartering/SellboxHandler.cs
@@ -18,6 +18,7 @@ public class SellboxHandler : MonoBehaviour
     {
         foreach (Transform g in transform.GetComponentsInChildren<Transform>())
         {
+            // if the child object has a SellBoxSlotHandler component, add it to the "slots" list.
             if (g.GetComponent<SellboxSlotHandler>())
                 slots.Add(g.gameObject);
         }
@@ -40,15 +41,21 @@ public class SellboxHandler : MonoBehaviour
 
     public void AddItemToBox(Item item)
     {
+        // add item to list
         itemsInBox.Add(item);
+        // Set the first free slot's item to the item you're selling
         slots[GetFirstFreeIndex()].GetComponent<SellboxSlotHandler>().SetSlot(item);
+        // add to currentAmountInBox
         currentAmountInBox++;
+        // add item's value to total value
         value += item.data.SellPrice * item.amount;
+        // update value text
         valueText.text = $"Value: {value} credits";
     }
 
     public void AddQuantityToBox(ItemData item, int amount)
     {
+        // this method does the same as AddItemToBox, but it uses a custom amount
         itemsInBox.Add(new Item(item, amount));
         slots[GetFirstFreeIndex()].GetComponent<SellboxSlotHandler>().SetSlot(new Item(item, amount));
         currentAmountInBox++;
@@ -58,20 +65,31 @@ public class SellboxHandler : MonoBehaviour
 
     public void RemoveItemFromBox(GameObject slot)
     {
+        // Get the item from the slot the player clicked
         Item item = slot.GetComponent<SellboxSlotHandler>().GetItem();
+        // safeguarding, if the itemsinbox list contains the item
         if (itemsInBox.Contains(item))
         {
+            // clear the slot
             slots[slots.IndexOf(slot)].GetComponent<SellboxSlotHandler>().ClearSlot();
+            // remove the item from the list
             itemsInBox.Remove(item);
             currentAmountInBox--;
+            // lower value
             value -= item.data.SellPrice * item.amount;
+            // update the value text
             valueText.text = $"Value: {value} credits";
         }
 
     }
 
+    /// <summary>
+    /// Gets the index of the first free slot
+    /// </summary>
+    /// <returns>index of the first free slot, or int.MaxValue if none are free</returns>
     public int GetFirstFreeIndex()
     {
+        // loop through the list, and return the index of the first instance where the isTaken variable of SellBoxSlotHandler is false
         for (int i = 0; i < slots.Count; i++)
         {
             GameObject slot = slots[i];
@@ -81,9 +99,13 @@ public class SellboxHandler : MonoBehaviour
         return int.MaxValue;
     }
 
+    /// <summary>
+    /// Sells all items in the sell box
+    /// </summary>
     public void SellItems()
     {
         EventChannels.BarteringEvents.OnPlayerSellsItems?.Invoke(value);
+        // Loops through the list, clears the items from all slots and adds the correct amount of credits to the player inventory
         for (int i = 0; i < capacity; i++)
         {
             slots[i].GetComponent<SellboxSlotHandler>().ClearSlot();
